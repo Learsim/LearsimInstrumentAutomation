5ed5e5147c0570666262de373116f4a3b0b538e7
axdra <axel.draws@outlook.com>
2020-12-20 15:27:11 +0100

    Updated so Updater writes steps

--- Upd.ps1
+++ Upd.ps1
@@ -1,10 +1,12 @@
-#Getting newest source from github
+
+Write-Output "Removing old files"
 $files = Get-ChildItem .
 foreach ($file in $files){
     if($file.name -ne "Upd.ps1" -And $file.name -ne "temp" -And $file.name -ne "config.json"){
         Remove-Item -Recurse -Force $file
     }
 }
+Write-Output "Copying new files"
 #Copy Files
 Copy-Item -Path ".\temp\Update.bat" -Destination "."
 Copy-Item -Path ".\temp\requirements.txt" -Destination "."
@@ -13,6 +15,8 @@ Copy-Item -Path ".\temp\static" -Destination "." -Recurse
 Copy-Item -Path ".\temp\assets" -Destination "." -Recurse
 Copy-Item -Path ".\temp\templates" -Destination "." -Recurse
 Copy-Item -Path ".\temp\InstrumentAutomation.py" -Destination "."
+Write-Output "Installing pip packages (Can error but its okay.)"
 pip install -q -r requirements.txt
+Write-Output "Cleaning up...."
 Remove-Item -Force "requirements.txt"
 Remove-Item -Recurse -Force .\temp
--- Update.bat
+++ Update.bat
@@ -1,4 +1,7 @@
-git clone https://github.com/axdra/LearsimInstrumentAutomation temp
+@echo off
+echo Getting newest source from github
+git clone -q https://github.com/axdra/LearsimInstrumentAutomation temp
 if exist .\Upd.ps1 del .\Upd.ps1
-xcopy /F temp\Upd.ps1 .\Upd.ps1*
+xcopy /Q /F temp\Upd.ps1 .\Upd.ps1*
+echo Running update script
 powershell -ExecutionPolicy Bypass -File .\Upd.ps1
