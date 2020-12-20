c54950b33b05bddfafb94fe1db5c3b85247b7962
axdra <axel.draws@outlook.com>
2020-12-20 13:48:51 +0100

    Updated updater to powershell

---
+++ Update.ps1
@@ -0,0 +1,18 @@
+#Getting newest source from github
+git clone https://github.com/axdra/LearsimInstrumentAutomation temp
+$files = Get-ChildItem .
+foreach ($file in $files){
+    if($file.name -ne "Update.ps1" -And $file.name -ne "temp" -And $file.name -ne "config.json"){
+        Remove-Item -Recurse -Force $file
+    }
+}
+Copy-Item -Path ".\temp\requirements.txt" -Destination "."
+Copy-Item -Path ".\temp\config_example.json" -Destination "."
+Copy-Item -Path ".\temp\static" -Destination "." -Recurse
+Copy-Item -Path ".\temp\assets" -Destination "." -Recurse
+Copy-Item -Path ".\temp\templates" -Destination "." -Recurse
+Copy-Item -Path ".\temp\InstrumentAutomation.py" -Destination "."
+pip install -r requirements.txt
+Remove-Item -Force "requirements.txt"
+Copy-Item -Path ".\temp\Update.ps1" -Destination "."
+Remove-Item -Recurse -Force .\temp
